c83aa951ff0ba19cc16931730bd210c1f8a15f16
piratafivorxd <piratafivor@gmail.com>
2025-09-28 15:10:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('wheelCanvas');
 const ctx = canvas.getContext('2d');
-const participants = [ "AGUSTINA","JAIME","LEANDRO","SOLEDAD","PORFIRIO"];
-const winners = ["AGUSTINA" , "JAIME","SOLEDAD" ];
+const participants = [ "AGUSTINA","JAIME","LEANDRO","PORFIRIO"];
+const winners = ["AGUSTINA" , "JAIME","LEANDRO" ];
 let spinCount = 0;
 let isSpinning = false;
 
